a80921edb898c6a6311382c00fb54a763a77ba26
Grejdan Mirolena <mirolenavioleta.mg@gmail.com>
2022-05-17 02:33:03 +0300

    commit_3

--- homepage.js
+++ homepage.js
@@ -1,28 +1,58 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 1,
+    interval;
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function stopAndStart() {
+    if (interval) {
+        clearInterval(interval)
+    }
+    interval = setInterval(showSlides, 10000); 
 }
 
-// Thumbnail image controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+    stopAndStart()
+}
+
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
+    stopAndStart()
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+    var i,
+        slides = document.getElementsByClassName("mySlides"),
+        dots = document.getElementsByClassName("dot");
+
+    if (n === undefined) {
+        n = ++slideIndex;
+    }
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+stopAndStart();
+
+//Sidenav
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgb(254, 250, 224,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "#fefae0";
 }

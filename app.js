87abfa84373cbe660468d0c017a6f8f50b21f18f
Grejdan Mirolena <mirolenavioleta.mg@gmail.com>
2022-05-18 16:59:27 +0300

    commit-18.05

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express();
+const path = require('path');
+app.get('/homepage', function (req, res) {
+    res.sendFile('homepage.html', { root: path.join(__dirname) });
+});
+app.get('/about', function (req, res) {
+    res.sendFile('about.html', { root: path.join(__dirname) });
+});
+app.get('/login', function (req, res) {
+    res.sendFile('login.html', { root: path.join(__dirname) });
+});
+app.get('/contact', function (req, res) {
+    res.sendFile('contact.html', { root: path.join(__dirname) });
+});
+app.get('/breakfast/asian', function (req, res) {
+    res.sendFile('asian.html', { root: path.join(__dirname) });
+});
+app.get('/breakfast/european', function (req, res) {
+    res.sendFile('european.html', { root: path.join(__dirname) });
+});
+app.get('/breakfast', function (req, res) {
+    res.sendFile('american.html', { root: path.join(__dirname) });
+});
+app.get('/lunch/asian', function (req, res) {
+    res.sendFile('asian.html', { root: path.join(__dirname) });
+});
+app.get('/lunch/european', function (req, res) {
+    res.sendFile('european.html', { root: path.join(__dirname) });
+});
+app.get('/lunch/american', function (req, res) {
+    res.sendFile('american.html', { root: path.join(__dirname) });
+});
+app.get('/dinner/asian', function (req, res) {
+    res.sendFile('asian.html', { root: path.join(__dirname) });
+});
+app.get('/dinner/asian', function (req, res) {
+    res.sendFile('european.html', { root: path.join(__dirname) });
+});
+app.get('/dinner/asian', function (req, res) {
+    res.sendFile('american.html', { root: path.join(__dirname) });
+});
+app.use('/Proiect', express.static(path.join(__dirname)));
+app.use('lunch', express.static(path.join(__dirname)));
+app.use('breakfast', express.static(path.join(__dirname)));
+app.use('dinner', express.static(path.join(__dirname)));
+app.listen(5000);
---
+++ login.js
@@ -0,0 +1,16 @@
+
+// login
+var modal = document.getElementById('id01');
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// sign in
+var modal = document.getElementById('id02');
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}

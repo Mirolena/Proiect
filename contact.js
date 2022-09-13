7b360d52e2449618bc66f0713cd90dfe4075d75a
Grejdan Mirolena <mirolenavioleta.mg@gmail.com>
2022-09-14 01:59:38 +0300

    commit_7

---
+++ contact.js
@@ -0,0 +1,7 @@
+const myTimeout = setTimeout(about, 4000);
+
+function about() {
+  document.getElementById("demo1").innerHTML = "Numar de telefon : +40764057265"
+  document.getElementById("demo2").innerHTML = "Adresa de e-mail : ww_meals@gmail.com"
+  document.getElementById("demo3").innerHTML = "Pagina de facebook : @WwMeals"
+}
